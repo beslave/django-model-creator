b02ca2e94250ea26805ecc238ea7b972e76639f5
Vladislav Lozhechnik <lozhechnik.vladislav@gmail.com>
2014-10-07 18:04:30 +0300

    Fix model registration

--- manage.py
+++ manage.py
@@ -1,15 +1,10 @@
 #!/usr/bin/env python
 import os
 import sys
-import django
 
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "demo.settings")
-    django.setup()
-
-    from model_creator.logic import register_models_from_templates
-    register_models_from_templates()
 
     from django.core.management import execute_from_command_line
     execute_from_command_line(sys.argv)
